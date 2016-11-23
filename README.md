e0664d478a13311c94a54f40a83a131053814666
ram0973 <ram0973@gmail.com>
2016-11-23 21:11:13 +0300

    mass fixes

--- README.md
+++ README.md
@@ -1,64 +1,26 @@
-# Решение задачи [№17](https://devman.org/challenges/17/) с сайта [devman.org](https://devman.org)
-
-## Условие задачи:
-
-Сайт недостаточно сделать и запустить. 
-Нужно также обеспечить его работоспособность.
-
-Добиться этого не всегда просто, здесь не обойтись без средств мониторинга. 
-Как говорится, “кто предупреждён, тот вооружён".
-
-Давай создадим утилиту для проверки состояния наших сайтов. 
-На входе - текстовый файл с URL адресами для проверки. 
-На выходе - статус каждого сайта по результатам следующих проверок:
-
-сервер отвечает на запрос статусом HTTP 200;
-доменное имя сайта проплачено как минимум на 1 месяц вперед.
-С чего начать поиск решения:
-
-[Статья про Хуиз на Википедии](https://ru.wikipedia.org/wiki/WHOIS)
-[Статья про статусы ХТТП на Википедии](https://ru.wikipedia.org/wiki/Список_кодов_состояния_HTTP)
-
-## Системные требования
-
+# Task [№17](https://devman.org/challenges/17/) from [devman](https://devman.org)
+## Requirements
 ```
 Python 3.5.2+
-win-unicode-console
 python-whois
 requests
 python-dateutil
 chardet
 tqdm
 ```
-
-## Установка
-
-Windows
-
-```    
+## Setup
+```
 git clone https://github.com/ram0973/17_sites_monitoring.git
 cd 17_sites_monitoring
-(Windows) pip install -r requirements.txt
-(Linux) pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 ```
-    
 ## Описание работы
-
-Указываем путь к файлу со списком url, которые надо проверить.
-Url должны быть **обязательно** со схемой, например: https://
-Можно использовать домены второго уровня и ниже (третьего и т.д.).
-Выводится список: url сайта, отвечает ли сайт кодом HTTP 200,
-дата истечения оплаты домена, оплачен ли домен хотя бы на месяц.
-При желании можно перехватить вывод в файл через > или >> 
-(или дописать вывод в excel).
-
-## Запуск
-
-```
-(Windows) python check_sites_health.py --i путь_до_файла_с_url_сайтов
-(Linux) python3 check_sites_health.py --i путь_до_файла_с_url_сайтов
-```
- 
-## Лицензия
-
+Specify the path to the file with the list of site urls, which should be checked.
+Url must necessarily be with scheme, for example: https://
+It's possible to use second-level domains and below (third, etc.).
+Prints: url site, does the site responds with HTTP 200 code,
+domain expiration date, whether the domain is paid for at least a month.
+## Usage
+python3 check_sites_health.py --i file_with_urls
+## License
 [MIT](http://opensource.org/licenses/MIT)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,44 +1,17 @@
 # -*- coding: utf-8 -*-
 import argparse
-import socket
-
 import chardet
 import requests
-import sys
 from tqdm import tqdm
 from whois import whois
 from datetime import datetime
 from urllib.parse import urlparse
 from dateutil.relativedelta import relativedelta
 
-CONNECT_TIMEOUT = 9
-READ_TIMEOUT = 9
+TIMEOUTS = (9, 9)
 
 
-def enable_unicode_in_windows():
-    if sys.platform == 'win32':
-        import win_unicode_console
-        win_unicode_console.enable()
-
-
-def handle_os_error(decorated):
-    """ Декоратор исключений OSError, ошибка - выводим результат и выходим """
-    def decorator(*args, **kwargs):
-        try:
-            return decorated(*args, **kwargs)
-        except OSError as error:
-            print('Ошибка: %s в файле: %s' % (error.strerror, error.filename))
-            exit(1)
-    return decorator
-
-
-@handle_os_error
 def load_urls4check(path: str) -> list:
-    """
-    Загружаем url сайтов из файла
-    :param path: путь к файлу с url сайтов неизвестной кодировки
-    :return список непустых url без табуляции, пробелов, переводов строк
-    """
     with open(path, mode='rb') as binary_file:
         file_encoding = chardet.detect(binary_file.read())['encoding']
     with open(path, mode='r', encoding=file_encoding) as urls:
@@ -46,29 +19,18 @@ def load_urls4check(path: str) -> list:
 
 
 def is_server_respond_with_200(url: str) -> bool:
-    """ Проверяем, отвечает ли веб-сервер кодом статуса 200 """
-    return requests.get(url, timeout=(
-        CONNECT_TIMEOUT, READ_TIMEOUT)).status_code == 200
+    return requests.get(url, timeout=TIMEOUTS).status_code == 200
 
 
 def get_domain_expiration_date(domain_name: str) -> datetime:
-    """ Получаем дату окончания срока оплаты доменного имени """
-    # информация от whois отдаётся только по домену 2 уровня
     domain_name = '.'.join(domain_name.split('.')[-2:])
     who_is = whois(domain_name).expiration_date
     return who_is[0] if isinstance(who_is, list) else who_is
 
 
 def get_site_info(url: str) -> tuple:
-    """
-    Получаем информацию по сайту
-    :param url: url сайта
-    :return: кортеж (url, url_valid, status_200, exp_date, is_paid)
-    is_paid = true, если домен оплачен хотя бы на месяц
-    """
     url_parts = urlparse(url)
     url_valid = url_parts.scheme and url_parts.netloc
-    # тут не стал добавлять http:// если его нет в url не знаю корректно ли это
     if not url_valid:
         return url, False, None, None, None
     try:
@@ -76,60 +38,38 @@ def get_site_info(url: str) -> tuple:
     except (requests.ConnectionError, requests.Timeout,
             requests.TooManyRedirects):
         status_200 = None
-
     exp_date = get_domain_expiration_date(urlparse(url).netloc)
     if exp_date:
         is_paid = relativedelta(exp_date, datetime.today()).months > 0
     else:
         exp_date = None
         is_paid = None
-
-    # не знаю, праильно ли тут использовать кортеж
     return url, url_valid, status_200, exp_date, is_paid
 
 
 def print_site_info(site: tuple):
-    """ Печатаем информацию по сайту """
     url, url_valid, status_200, exp_date, is_paid = site
     if not url_valid:
-        print('Возможно, неверный url: %s' % url)
+        print('Maybe, wrong url: %s' % url)
         return
-    status_200 = '-' if status_200 is None else ['Нет', 'Да'][status_200]
+    status_200 = '-' if status_200 is None else ['No', 'Yes'][status_200]
     exp_date = '-' if exp_date is None else exp_date.strftime("%d.%m.%y")
-    is_paid = '-' if is_paid is None else ['Нет', 'Да'][is_paid]
+    is_paid = '-' if is_paid is None else ['No', 'Yes'][is_paid]
     print('{:50}{:^10}{:^11}{:^9}'.format(url, status_200, exp_date, is_paid))
 
-
-def get_args_parser():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Проверка статуса сайтов и оплаты доменного имени')
-    parser.add_argument('--file', '--f', help='Путь к файлу с url сайтов',
-                        # type=argparse.FileType(mode='r',),
-                        # required=True
-                        )
-    return parser
-
-
-def main():
-    enable_unicode_in_windows()
-    args_parser = get_args_parser()
-    urls_file_path = args_parser.parse_args().file
-    if not urls_file_path:
-        print('Ошибка: вы не задали путь к файлу с url сайтов')
-        args_parser.print_usage()
-        exit(1)
-
+        description='Checking sites status and their domain name payment')
+    parser.add_argument('--file', '--f', help='Path to file with sites urls',
+                        required=True)
+    urls_file_path = parser.parse_args().file
     site_urls = load_urls4check(urls_file_path)
 
-    print('\nПроверка статуса сайтов и оплаты доменного имени\n')
-    print('Получаем данные по сайтам из файла %s' % urls_file_path)
+    print('\nChecking sites status and their domain name payment\n')
+    print('Fetching data on sites by urls from file %s' % urls_file_path)
     sites_info = [get_site_info(site_url) for site_url in tqdm(site_urls)]
 
     print('\n{:50}{}{}{}'.format(
-        'Сайт', ' Отвечает ', 'Оплачен до, ', 'за месяц'))
+        'Site', ' Responds ', 'Paid until ', 'For month'))
     for site_info in sites_info:
         print_site_info(site_info)
-
-
-if __name__ == '__main__':
-    main()
