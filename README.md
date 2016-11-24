5159241b75fda1b2d9d911a8709ad13c535ca13c
ram0973 <ram0973@gmail.com>
2016-11-24 12:46:57 +0300

    errors fixed

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Task [№17](https://devman.org/challenges/17/) from [devman](https://devman.org)
+## What is it
+
 ## Requirements
 ```
 Python 3.5.2+
@@ -14,13 +16,13 @@ git clone https://github.com/ram0973/17_sites_monitoring.git
 cd 17_sites_monitoring
 pip3 install -r requirements.txt
 ```
-## Описание работы
+## Usage
 Specify the path to the file with the list of site urls, which should be checked.
 Url must necessarily be with scheme, for example: https://
 It's possible to use second-level domains and below (third, etc.).
 Prints: url site, does the site responds with HTTP 200 code,
 domain expiration date, whether the domain is paid for at least a month.
-## Usage
+## Run
 python3 check_sites_health.py --i file_with_urls
 ## License
 [MIT](http://opensource.org/licenses/MIT)
--- check_sites_health.py
+++ check_sites_health.py
@@ -11,28 +11,28 @@ from dateutil.relativedelta import relativedelta
 TIMEOUTS = (9, 9)
 
 
-def load_urls4check(path: str) -> list:
+def load_urls4check(path):
     with open(path, mode='rb') as binary_file:
         file_encoding = chardet.detect(binary_file.read())['encoding']
     with open(path, mode='r', encoding=file_encoding) as urls:
-        return [url.strip(' \t\n\r') for url in urls if url.strip(' \t\n\r')]
+        return [url.strip() for url in urls if url.strip()]
 
 
-def is_server_respond_with_200(url: str) -> bool:
+def is_server_respond_with_200(url):
     return requests.get(url, timeout=TIMEOUTS).status_code == 200
 
 
-def get_domain_expiration_date(domain_name: str) -> datetime:
-    domain_name = '.'.join(domain_name.split('.')[-2:])
-    who_is = whois(domain_name).expiration_date
+def get_domain_expiration_date(domain_name):
+    who_is = whois('.'.join(domain_name.split('.')[-2:])).expiration_date
     return who_is[0] if isinstance(who_is, list) else who_is
 
 
-def get_site_info(url: str) -> tuple:
+def get_site_info(url):
     url_parts = urlparse(url)
     url_valid = url_parts.scheme and url_parts.netloc
     if not url_valid:
-        return url, False, None, None, None
+        return {'url': url, 'url_valid': False, 'status_200': None,
+                'exp_date': None, 'is_paid': None}
     try:
         status_200 = is_server_respond_with_200(url)
     except (requests.ConnectionError, requests.Timeout,
@@ -44,14 +44,18 @@ def get_site_info(url: str) -> tuple:
     else:
         exp_date = None
         is_paid = None
-    return url, url_valid, status_200, exp_date, is_paid
+    return {'url': url, 'url_valid': url_valid, 'status_200': status_200,
+            'exp_date': exp_date, 'is_paid': is_paid}
 
 
-def print_site_info(site: tuple):
-    url, url_valid, status_200, exp_date, is_paid = site
-    if not url_valid:
+def print_site_info(site):
+    url = site['url']
+    if not site['url_valid']:
         print('Maybe, wrong url: %s' % url)
         return
+    status_200 = site['status200']
+    exp_date = site['exp_date']
+    is_paid = site['is_paid']
     status_200 = '-' if status_200 is None else ['No', 'Yes'][status_200]
     exp_date = '-' if exp_date is None else exp_date.strftime("%d.%m.%y")
     is_paid = '-' if is_paid is None else ['No', 'Yes'][is_paid]
@@ -64,11 +68,9 @@ if __name__ == '__main__':
                         required=True)
     urls_file_path = parser.parse_args().file
     site_urls = load_urls4check(urls_file_path)
-
     print('\nChecking sites status and their domain name payment\n')
     print('Fetching data on sites by urls from file %s' % urls_file_path)
     sites_info = [get_site_info(site_url) for site_url in tqdm(site_urls)]
-
     print('\n{:50}{}{}{}'.format(
         'Site', ' Responds ', 'Paid until ', 'For month'))
     for site_info in sites_info:
